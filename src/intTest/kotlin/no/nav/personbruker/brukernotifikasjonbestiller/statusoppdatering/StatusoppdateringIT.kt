2ef8e9de81467815da0bbebe6be1bba6fb356a8c
Christopher Santana <christopher.santana@nav.no>
2021-04-22 11:56:03 +0200

    Utvidet integrasjonstester med sjekk mot feilrespons-topic.

--- src/intTest/kotlin/no/nav/personbruker/brukernotifikasjonbestiller/statusoppdatering/StatusoppdateringIT.kt
+++ src/intTest/kotlin/no/nav/personbruker/brukernotifikasjonbestiller/statusoppdatering/StatusoppdateringIT.kt
@@ -25,7 +25,7 @@ import no.nav.personbruker.dittnav.brukernotifikasjonbestiller.config.Kafka
 import no.nav.personbruker.dittnav.brukernotifikasjonbestiller.metrics.MetricsCollector
 import no.nav.personbruker.dittnav.brukernotifikasjonbestiller.metrics.ProducerNameResolver
 import no.nav.personbruker.dittnav.brukernotifikasjonbestiller.metrics.ProducerNameScrubber
-import no.nav.personbruker.dittnav.brukernotifikasjonbestiller.nokkel.AvroNokkelObjectMother
+import no.nav.personbruker.dittnav.brukernotifikasjonbestiller.nokkel.AvroNokkelObjectMother.createNokkelWithEventId
 import no.nav.personbruker.dittnav.brukernotifikasjonbestiller.statusoppdatering.AvroStatusoppdateringObjectMother
 import no.nav.personbruker.dittnav.brukernotifikasjonbestiller.statusoppdatering.StatusoppdateringEventService
 import no.nav.personbruker.dittnav.common.metrics.StubMetricsReporter
@@ -40,14 +40,19 @@ import org.junit.jupiter.api.TestInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class StatusoppdateringIT {
-    private val embeddedEnv = KafkaTestUtil.createDefaultKafkaEmbeddedInstance(listOf(Kafka.statusoppdateringInputTopicName, Kafka.statusoppdateringHovedTopicName))
+    private val embeddedEnv = KafkaTestUtil.createDefaultKafkaEmbeddedInstance(listOf(Kafka.statusoppdateringInputTopicName, Kafka.statusoppdateringHovedTopicName, Kafka.feilresponsTopicName))
     private val testEnvironment = KafkaTestUtil.createEnvironmentForEmbeddedKafka(embeddedEnv)
 
     private val database = H2Database()
 
-    private val statusoppdateringEvents = (1..10).map { AvroNokkelObjectMother.createNokkelWithEventId(it.toString()) to AvroStatusoppdateringObjectMother.createStatusoppdateringWithGrupperingsId(it.toString()) }.toMap()
+    private val goodEvents = createEvents(10)
+    private val badEvent = createEventWithTooLongGroupId("bad")
+    private val statusoppdateringEvents = goodEvents.toMutableList().apply {
+        add(badEvent)
+    }.toMap()
 
     private val capturedInternalRecords = ArrayList<RecordKeyValueWrapper<NokkelIntern, StatusoppdateringIntern>>()
+    private val capturedErrorResponseRecords = ArrayList<RecordKeyValueWrapper<NokkelFeilrespons, Feilrespons>>()
 
     private val producerNameAlias = "dittnav"
     private val client = getClient(producerNameAlias)
@@ -72,16 +77,17 @@ class StatusoppdateringIT {
     }
 
     @Test
-    fun `Should read Statusoppdatering-events and send to hoved-topic`() {
+    fun `Should read Statusoppdatering-events and send to hoved-topic or error response topic as appropriate`() {
         runBlocking {
             KafkaTestUtil.produceEvents(testEnvironment, Kafka.statusoppdateringInputTopicName, statusoppdateringEvents)
         } shouldBeEqualTo true
 
         `Read all Statusoppdatering-events from our input-topic and verify that they have been sent to the main-topic`()
 
-        statusoppdateringEvents.size `should be equal to` capturedInternalRecords.size
-
+        capturedInternalRecords.size `should be equal to` goodEvents.size
+        capturedErrorResponseRecords.size `should be equal to` 1
     }
+
 
     fun `Read all Statusoppdatering-events from our input-topic and verify that they have been sent to the main-topic`() {
         val consumerProps = KafkaEmbed.consumerProps(testEnvironment, Eventtype.STATUSOPPDATERING, true)
@@ -103,10 +109,12 @@ class StatusoppdateringIT {
         val consumer = Consumer(Kafka.statusoppdateringInputTopicName, kafkaConsumer, eventService)
 
         internalKafkaProducer.initTransactions()
+        feilresponsKafkaProducer.initTransactions()
         runBlocking {
             consumer.startPolling()
 
             `Wait until all statusoppdatering events have been received by target topic`()
+            `Wait until bad event has been received by error topic`()
 
             consumer.stopPolling()
         }
@@ -123,7 +131,7 @@ class StatusoppdateringIT {
 
         targetConsumer.startPolling()
 
-        while (currentNumberOfRecords < statusoppdateringEvents.size) {
+        while (currentNumberOfRecords < goodEvents.size) {
             runBlocking {
                 currentNumberOfRecords = capturingProcessor.getEvents().size
                 delay(100)
@@ -135,5 +143,41 @@ class StatusoppdateringIT {
         }
 
         capturedInternalRecords.addAll(capturingProcessor.getEvents())
+    }
+
+
+    private fun `Wait until bad event has been received by error topic`() {
+        val targetConsumerProps = KafkaEmbed.consumerProps(testEnvironment, Eventtype.FEILRESPONS, true)
+        val targetKafkaConsumer = KafkaConsumer<NokkelFeilrespons, Feilrespons>(targetConsumerProps)
+        val capturingProcessor = CapturingEventProcessor<NokkelFeilrespons, Feilrespons>()
+
+        val targetConsumer = Consumer(Kafka.feilresponsTopicName, targetKafkaConsumer, capturingProcessor)
+
+        var receivedEvent = false
+
+        targetConsumer.startPolling()
+
+        while (!receivedEvent) {
+            runBlocking {
+                receivedEvent = capturingProcessor.getEvents().isNotEmpty()
+                delay(100)
+            }
+        }
+
+        runBlocking {
+            targetConsumer.stopPolling()
+        }
+
+        capturedErrorResponseRecords.addAll(capturingProcessor.getEvents())
+    }
+
+    private fun createEvents(number: Int) = (1..number).map {
+        createNokkelWithEventId(it.toString()) to AvroStatusoppdateringObjectMother.createStatusoppdateringWithGrupperingsId(it.toString())
+    }
+
+    private fun createEventWithTooLongGroupId(eventId: String): Pair<Nokkel, Statusoppdatering> {
+        val groupId = "groupId".repeat(100)
+
+        return createNokkelWithEventId(eventId) to AvroStatusoppdateringObjectMother.createStatusoppdateringWithGrupperingsId(groupId)
     }
 }
