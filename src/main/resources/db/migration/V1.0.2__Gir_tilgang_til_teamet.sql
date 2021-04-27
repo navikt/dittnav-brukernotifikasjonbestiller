73195f8ebc8b5d02b4bb86bbbca11ef5c99de838
Vanessa Kerr <Vanessa.kjolner.kerr@nav.no>
2021-04-27 15:51:21 +0200

    Lagt til grant usage on schema, for å få tilgang til eksisterende tabeller.

---
+++ src/main/resources/db/migration/V1.0.2__Gir_tilgang_til_teamet.sql
@@ -0,0 +1,15 @@
+DO
+$$
+    BEGIN
+        IF EXISTS
+            (SELECT 1 from pg_roles where rolname = 'cloudsqliamuser')
+        THEN
+            GRANT USAGE ON SCHEMA public TO cloudsqliamuser;
+            GRANT USAGE ON SCHEMA brukernotifikasjonbestilling TO cloudsqliamuser;
+            GRANT ALL ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
+            GRANT ALL ON ALL TABLES IN SCHEMA brukernotifikasjonbestilling TO cloudsqliamuser;
+            ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO cloudsqliamuser;
+            ALTER DEFAULT PRIVILEGES IN SCHEMA brukernotifikasjonbestilling GRANT ALL ON TABLES TO cloudsqliamuser;
+        END IF;
+    END
+$$;
