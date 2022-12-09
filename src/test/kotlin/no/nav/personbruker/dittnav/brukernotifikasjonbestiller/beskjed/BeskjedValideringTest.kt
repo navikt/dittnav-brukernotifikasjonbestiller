c388024c271eccc4582808d41cdc6be3b907833c
Atle Nordland <atle.nordland@nav.no>
2022-12-09 15:37:00 +0100

    Refactor: rename validatorer

--- src/test/kotlin/no/nav/personbruker/dittnav/brukernotifikasjonbestiller/beskjed/BeskjedValideringTest.kt
+++ src/test/kotlin/no/nav/personbruker/dittnav/brukernotifikasjonbestiller/beskjed/BeskjedValideringTest.kt
@@ -11,7 +11,7 @@ class BeskjedValideringTest {
 
     @Test
     fun `beskjed med gyldige felter er gyldig`() {
-        val validation = BeskjedValidation(
+        val validation = VarselValidation(
             BeskjedTestData.beskjedInput(
                 tekst = "x".repeat(300),
                 link = "https://" + "x".repeat(192),
@@ -25,7 +25,7 @@ class BeskjedValideringTest {
 
     @Test
     fun `valgfrie felter kan være null`() {
-        val validation = BeskjedValidation(
+        val validation = VarselValidation(
             BeskjedTestData.beskjedInput(
                 //sikkerhetsnivaa = null,
                 link = null,
@@ -40,7 +40,7 @@ class BeskjedValideringTest {
 
     @Test
     fun `obligatoriske felter kan ikke være null`() {
-        val validation = BeskjedValidation(
+        val validation = VarselValidation(
             BeskjedTestData.beskjedInput(
                 tekst = null
             )
@@ -48,115 +48,115 @@ class BeskjedValideringTest {
         validation.apply {
             isValid() shouldBe false
             failedValidators.map { it.javaClass } shouldContainExactlyInAnyOrder listOf(
-                TekstIsMax300Characters::class.java
+                TekstValidator::class.java
             )
         }
     }
 
     @Test
     fun `tekst kan maks være 300 tegn`() {
-        val validation = BeskjedValidation(
+        val validation = VarselValidation(
             BeskjedTestData.beskjedInput(
                 tekst = "x".repeat(301)
             )
         )
         validation.isValid() shouldBe false
-        validation.failedValidators.map { it.javaClass } shouldContainExactlyInAnyOrder listOf(
-            TekstIsMax300Characters::class.java
+        validation.failedValidators.map { it.javaClass } shouldBe listOf(
+            TekstValidator::class.java
         )
     }
 
     @Test
     fun `optional link må være gyldig lenke og maks 200 tegn`() {
-        BeskjedValidation(
+        VarselValidation(
             BeskjedTestData.beskjedInput(
                 link = "https://" + "x".repeat(193)
             )
         ).apply {
             isValid() shouldBe false
             failedValidators.map { it.javaClass } shouldBe listOf(
-                LinkIsURLandMax200Characters::class.java
+                LinkValidator::class.java
             )
         }
 
-        BeskjedValidation(
+        VarselValidation(
             BeskjedTestData.beskjedInput(
                 link = "ugyldig-link"
             )
         ).apply {
             isValid() shouldBe false
             failedValidators.map { it.javaClass } shouldBe listOf(
-                LinkIsURLandMax200Characters::class.java
+                LinkValidator::class.java
             )
         }
     }
 
     @Test
     fun `sikkerhetsnivaa må være 3 eller 4`() {
-        val validation = BeskjedValidation(
+        val validation = VarselValidation(
             BeskjedTestData.beskjedInput(
                 sikkerhetsnivaa = 5
             )
         )
         validation.isValid() shouldBe false
         validation.failedValidators.map { it.javaClass } shouldBe listOf(
-            SikkerhetsnivaaIs3Or4::class.java
+            SikkerhetsnivaaValidator::class.java
         )
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["ABC", "SMS,ABC", ""])
     fun `optional prefererte kanaler må være SMS eller EPOST`(prefererteKanaler: String) {
-        val validation = BeskjedValidation(
+        val validation = VarselValidation(
             BeskjedTestData.beskjedInput(
                 prefererteKanaler = prefererteKanaler.split(",")
             )
         )
         validation.isValid() shouldBe false
         validation.failedValidators.map { it.javaClass } shouldBe listOf(
-            PrefererteKanalerIsSMSorEpost::class.java
+            PrefererteKanalerValidator::class.java
         )
     }
 
     @ParameterizedTest
     @ValueSource(ints = [0, 161])
     fun `optional smstekst kan ikke være tom, og maks 160 tegn`(length: Int) {
-        val validation = BeskjedValidation(
+        val validation = VarselValidation(
             BeskjedTestData.beskjedInput(
                 smsVarslingstekst = "x".repeat(length)
             )
         )
         validation.isValid() shouldBe false
         validation.failedValidators.map { it.javaClass } shouldBe listOf(
-            SmstekstIsMax160Characters::class.java
+            SmstekstValidator::class.java
         )
     }
 
     @ParameterizedTest
     @ValueSource(ints = [0, 4001])
     fun `optional eposttekst kan ikke være tom, og maks 4000 tegn`(length: Int) {
-        val validation = BeskjedValidation(
+        val validation = VarselValidation(
             BeskjedTestData.beskjedInput(
                 epostVarslingstekst = "x".repeat(length)
             )
         )
         validation.isValid() shouldBe false
         validation.failedValidators.map { it.javaClass } shouldBe listOf(
-            EposttekstIsMax4000Characters::class.java
+            EposttekstValidator::class.java
         )
     }
 
     @ParameterizedTest
     @ValueSource(ints = [0, 41])
     fun `optional eposttittel kan ikke være tom, og maks 40 tegn`(length: Int) {
-        val validation = BeskjedValidation(
+        val validation = VarselValidation(
             BeskjedTestData.beskjedInput(
                 epostVarslingstittel = "x".repeat(length)
             )
         )
         validation.isValid() shouldBe false
         validation.failedValidators.map { it.javaClass } shouldBe listOf(
-            EposttittelIsMax40Characters::class.java
+            EposttittelValidator::class.java
         )
     }
 
