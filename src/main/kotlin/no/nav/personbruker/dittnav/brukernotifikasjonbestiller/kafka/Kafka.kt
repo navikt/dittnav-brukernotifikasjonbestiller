ea114eaaa8500a9ee20c91f0fcabaa10f94ddc36
Christopher Santana <christopher.santana@nav.no>
2023-12-06 10:54:47 +0100

    Cleanup.

--- src/main/kotlin/no/nav/personbruker/dittnav/brukernotifikasjonbestiller/kafka/Kafka.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/brukernotifikasjonbestiller/kafka/Kafka.kt
@@ -7,15 +7,35 @@ import no.nav.personbruker.dittnav.brukernotifikasjonbestiller.config.Environmen
 import no.nav.personbruker.dittnav.brukernotifikasjonbestiller.config.SecurityVars
 import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.config.SaslConfigs
 import org.apache.kafka.common.config.SslConfigs
+import org.apache.kafka.common.serialization.StringSerializer
 import java.net.InetSocketAddress
 import java.util.*
 
 object Kafka {
 
-    fun consumerProps(env: Environment): Properties {
-        return Properties().apply {
+    fun createProducer(environment: Environment) = KafkaProducer<String, String>(
+        Properties().apply {
+            put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, environment.aivenBrokers)
+            put(
+                ProducerConfig.CLIENT_ID_CONFIG,
+                "dittnav-brukernotifikasjonbestiller"
+            )
+            put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
+            put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
+            put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 40000)
+            put(ProducerConfig.ACKS_CONFIG, "all")
+            put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true")
+            putAll(credentialProps(environment.securityConfig.variables!!))
+        }
+    )
+
+    fun <K, V> createConsumer(env: Environment) = KafkaConsumer<K, V>(
+        Properties().apply {
             put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, env.aivenBrokers)
             put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, env.aivenSchemaRegistry)
             put(ConsumerConfig.GROUP_ID_CONFIG, env.groupId)
@@ -26,12 +46,12 @@ object Kafka {
             put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SwallowSerializationErrorsAvroDeserializer::class.java)
             put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true)
             if (env.securityConfig.enabled) {
-                putAll(credentialPropsAiven(env.securityConfig.variables!!))
+                putAll(credentialProps(env.securityConfig.variables!!))
             }
         }
-    }
+    )
 
-    fun credentialPropsAiven(securityVars: SecurityVars): Properties {
+    private fun credentialProps(securityVars: SecurityVars): Properties {
         return Properties().apply {
             put(KafkaAvroSerializerConfig.USER_INFO_CONFIG, "${securityVars.aivenSchemaRegistryUser}:${securityVars.aivenSchemaRegistryPassword}")
             put(KafkaAvroSerializerConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO")
