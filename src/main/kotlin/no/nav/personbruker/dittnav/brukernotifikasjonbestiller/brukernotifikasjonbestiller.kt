4ecc1468af899b57e6bc66d56d8198ae87aedf07
Christopher Santana <christopher.santana@nav.no>
2024-12-13 15:00:05 +0100

    Tilpasser endringer i dependencies.

--- src/main/kotlin/no/nav/personbruker/dittnav/brukernotifikasjonbestiller/brukernotifikasjonbestiller.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/brukernotifikasjonbestiller/brukernotifikasjonbestiller.kt
@@ -3,7 +3,6 @@ package no.nav.personbruker.dittnav.brukernotifikasjonbestiller
 import io.ktor.server.application.*
 import io.ktor.server.plugins.defaultheaders.*
 import io.ktor.server.routing.*
-import io.prometheus.client.hotspot.DefaultExports
 import kotlinx.coroutines.runBlocking
 import no.nav.personbruker.dittnav.brukernotifikasjonbestiller.kafka.InputTopicConsumer
 import no.nav.personbruker.dittnav.brukernotifikasjonbestiller.config.AppHealthChecker
@@ -15,8 +14,6 @@ fun Application.brukernotifikasjonBestiller(
     varselActionProducer: VarselActionProducer,
     appHealthChecker: AppHealthChecker
 ) {
-    DefaultExports.initialize()
-
     install(DefaultHeaders)
 
     routing {
@@ -28,13 +25,13 @@ fun Application.brukernotifikasjonBestiller(
 }
 
 private fun Application.configureStartupHook(inputConsumer: InputTopicConsumer) {
-    environment.monitor.subscribe(ApplicationStarted) {
+    monitor.subscribe(ApplicationStarted) {
         inputConsumer.startPolling()
     }
 }
 
 private fun Application.configureShutdownHook(inputConsumer: InputTopicConsumer, varselActionProducer: VarselActionProducer) {
-    environment.monitor.subscribe(ApplicationStopPreparing) {
+    monitor.subscribe(ApplicationStopPreparing) {
         runBlocking {
             inputConsumer.stopPolling()
             varselActionProducer .flushAndClose()
--- src/main/kotlin/no/nav/personbruker/dittnav/brukernotifikasjonbestiller/config/healthApi.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/brukernotifikasjonbestiller/config/healthApi.kt
@@ -1,16 +1,15 @@
 package no.nav.personbruker.dittnav.brukernotifikasjonbestiller.config
 
 import io.ktor.http.*
-import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.get
-import io.prometheus.client.CollectorRegistry
-import io.prometheus.client.exporter.common.TextFormat
+import io.prometheus.metrics.expositionformats.ExpositionFormats
+import io.prometheus.metrics.model.registry.MetricNameFilter
+import io.prometheus.metrics.model.registry.PrometheusRegistry
 
 fun Routing.healthApi(
-    appHealthChecker: AppHealthChecker,
-    collectorRegistry: CollectorRegistry = CollectorRegistry.defaultRegistry
+    appHealthChecker: AppHealthChecker
 ) {
 
     get("/internal/isAlive") {
@@ -29,10 +28,21 @@ fun Routing.healthApi(
         call.respondText(text = "READY", contentType = ContentType.Text.Plain)
     }
 
+    val writer = ExpositionFormats.init().openMetricsTextFormatWriter
+
     get("/metrics") {
-        val names = call.request.queryParameters.getAll("name")?.toSet() ?: emptySet()
-        call.respondTextWriter(ContentType.parse(TextFormat.CONTENT_TYPE_004), HttpStatusCode.OK) {
-            TextFormat.write004(this, collectorRegistry.filteredMetricFamilySamples(names))
+        PrometheusRegistry.defaultRegistry.scrape()
+
+        val requestedNames = call.request.queryParameters.getAll("name[]")?.toSet() ?: emptySet()
+
+        val filter = if (requestedNames.isNotEmpty()) {
+            MetricNameFilter.builder().nameMustBeEqualTo(requestedNames).build()
+        } else {
+            null
+        }
+
+        call.respondOutputStream(ContentType.parse(writer.contentType)) {
+            writer.write(this, PrometheusRegistry.defaultRegistry.scrape(filter))
         }
     }
 }
